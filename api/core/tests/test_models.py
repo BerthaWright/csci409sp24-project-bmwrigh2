ac4ca141eb8b9fe3bf1f640caf1da0954a70ceb8
BerthaWright <74274285+BerthaWright@users.noreply.github.com>
2024-03-12 22:20:24 -0400

    Created Models

---
+++ api/core/tests/test_models.py
@@ -0,0 +1,66 @@
+"""
+Tests for models.
+"""
+
+from django.test import TestCase
+
+from core import models
+
+from datetime import datetime
+
+def create_vessel():
+    return models.Vessel.objects.create(
+        vessel_name = "Alaskan Voyager",
+)    
+
+class ModelTests(TestCase):
+
+    def test_create_vessel(self):
+        """Test Creating a vessel is Successful."""
+        vessel = models.Vessel.objects.create(
+            name="Alaskan Voyager",
+        )
+
+        self.assertEqual(str(vessel), vessel.name)
+
+    
+    def test_create_vesselschedule(self):
+        """Test Creating a vessel schedule is Successful."""
+        vesselschedule = models.VesselSchedule.objects.create(
+            vessel= create_vessel(),
+            voyage_number= 12345,
+            arrival_date=datetime.now(),
+        )
+        vesselschedule_result = "{}-{}.format(vessel, voyage_number)"
+       
+        self.assertEqual(str(vesselschedule), vesselschedule_result)
+
+#here
+    def test_create_billoflanding(self):
+        """Test Creating a Bill of landing is Successful."""
+        billoflanding = models.BillOfLanding.objects.create(
+            voyage=create_vessel(),
+            bol_number = 14328,
+            contact_name="John Smith",
+            contact_number = 8435679000,
+            contact_email="js@av.com",
+            release_status = "R",
+        )
+        runway_result = "{}{}.format(runway_number, runway_designation)"
+        
+        self.assertEqual(billoflanding, billoflanding.bol_number)
+
+    def test_create_container(self):
+        """Test Creating a container is Successful."""
+        billoland= models.BillOfLanding.objects.create(
+            voyage=create_vessel(),
+            bol_number = 14328,
+            contact_name="John Smith",
+            contact_number = 8435679000,
+            contact_email="js@av.com",
+            release_status = "R",
+        )
+        container = models.Container.objects.create(
+            bol= billoland,
+            container_number= 58439058
+        )
